24079df56c02d3ecf6a29ab8eff75eaf8c8e3852
rcoric3 <92095914+rcoric3@users.noreply.github.com>
2024-01-15 08:42:59 +0100

    Homescreen added new button with functionality

--- app/src/main/java/com/mygame/thesnakegame/HomeScreenActivity.java
+++ app/src/main/java/com/mygame/thesnakegame/HomeScreenActivity.java
@@ -2,8 +2,12 @@ package com.mygame.thesnakegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.thesnakegame.R;
 
@@ -15,5 +19,18 @@ public class HomeScreenActivity extends AppCompatActivity {
         setContentView(R.layout.home_screen);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageButton homeScreenButton = findViewById(R.id.myHomeScreenButton);
+        homeScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToNextScreen();
+            }
+        });
+    }
+
+    private void goToNextScreen() {
+        Intent intent = new Intent(this, GameScreenActivity.class);
+        startActivity(intent);
     }
 }
